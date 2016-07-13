9c8c72c772f383ee07e6718400498600eb5334c2
chucklapress <charleslapress@gmail.com>
2016-07-13 09:13:53 -0400

    added transfer model to allow transfer between accounts

--- ironapp/admin.py
+++ ironapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from ironapp.models import AcctBalance
+from ironapp.models import AcctBalance, Transfer
 
 
 # Register your models here.
 
 admin.site.register(AcctBalance)
+admin.site.register(Transfer)
--- ironapp/models.py
+++ ironapp/models.py
@@ -12,5 +12,16 @@ class AcctBalance(models.Model):
     date = models.DateTimeField(default=datetime.now, blank=True)
     customer = models.ForeignKey('auth.User')
     memo_or_note = models.CharField(max_length=35)
+    is_transfer = models.BooleanField(default=False)
+    def __str__(self):
+        return self.memo_or_note
+
+class Transfer(models.Model):
+    account_number = models.IntegerField()
+    transfer_amount = models.IntegerField()
+    from_account = models.OneToOneField('auth.User')
+    date =models.DateTimeField(default=datetime.now, blank=True)
+    customer = models.ForeignKey(AcctBalance)
+    memo_or_note = models.CharField(max_length=35)
     def __str__(self):
         return self.memo_or_note
